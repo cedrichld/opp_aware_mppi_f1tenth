cbbf34bfe549841bc9934c4bd367a12bbafdde93
Cedric Hollande <cedrich@seas.upenn.edu>
2026-06-22 15:21:30 -0400

    IV11, manage to not crash in that S before straight

--- mppi_bringup/launch/IV_main.launch.py
+++ mppi_bringup/launch/IV_main.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
     )
 
     pkg_share = get_package_share_directory('mppi_bringup')
-    csv_path = os.path.join(pkg_share, 'waypoints', 'IV9.csv')
+    csv_path = os.path.join(pkg_share, 'waypoints', 'IV11.csv')
 
     mppi_node = Node(
         package='mppi_example',
